30bb09e5e7759c5e1193560fad5abdfc6efd7e6d
Dennis Cho <yunseong3736@gmail.com>
2021-10-03 18:15:50 +0900

    Practice03 Edited
    Practice03 Edited

--- practice03/practice03_5/Sierpinski_Gasket.cpp
+++ practice03/practice03_5/Sierpinski_Gasket.cpp
@@ -1,15 +1,27 @@
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+using namespace std;
 
 /* 삼각형 초기화 */
 GLfloat v[3][2] = { {-1.0, -0.58}, {1.0, -0.58}, {0.0, 1.15} };
-int n = 0;// 반복 횟수
+int n = 10000;// 반복 횟수
 
+void triangle(void) {
+    GLfloat v1[2] = { -1.0, -0.58 };
+    for (int i = 0; i < n; i++) {
+        int j = rand() % 3;
+        v1[0] = (v1[0] + v[j][0]) / 2;
+        v1[1] = (v1[1] + v[j][1]) / 2;
+        glVertex2fv(v1);
+    }
+}
+
+/*
 // 좌표와 몇번 반복할지 받는 변수 매개변수
 void divide_triangle(GLfloat* a, GLfloat* b, GLfloat* c, int m)
 {
-    /* 꼭짓점 수를 이용한 삼각형 분할 */
     GLfloat v0[2], v1[2], v2[2];
     int j;
     if (m > 0)
@@ -19,9 +31,18 @@ void divide_triangle(GLfloat* a, GLfloat* b, GLfloat* c, int m)
             v1[j] = (a[j] + c[j]) / 2;
             v2[j] = (b[j] + c[j]) / 2;
         }
-        divide_triangle(a, v0, v1, m - 1);
-        divide_triangle(b, v2, v0, m - 1);
-        divide_triangle(c, v1, v2, m - 1);        
+        int r = rand() % 3;
+        switch (r) {
+        case 0:
+            divide_triangle(a, v0, v1, m - 1);
+            break;
+        case 1:
+            divide_triangle(b, v2, v0, m - 1);
+            break;
+        case 2:
+            divide_triangle(c, v1, v2, m - 1);
+            break;
+        }             
     }
     else {
         glVertex2fv(a);
@@ -29,21 +50,16 @@ void divide_triangle(GLfloat* a, GLfloat* b, GLfloat* c, int m)
         glVertex2fv(c);
     }
 }
-
+*/
 void display()
 {
     if (n == 0) return;
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    divide_triangle(v[0], v[1], v[2], n);
+    glBegin(GL_POINTS);
+    //divide_triangle(v[0], v[1], v[2], n);
+    triangle();
     glEnd();
     glFlush();
-}
-
-void MyMainMenu(int entryID) {
-    if (entryID == -1) exit(0);
-    n = entryID;
-    glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
@@ -56,22 +72,7 @@ int main(int argc, char** argv) {
     gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glColor3f(1.0, 1.0, 1.0);
-
-    //메뉴 생성 (1 to 10 and Exit)
-    GLint MyMainMenuID = glutCreateMenu(MyMainMenu);
-    glutAddMenuEntry("1", 1);
-    glutAddMenuEntry("2", 2);
-    glutAddMenuEntry("3", 3);
-    glutAddMenuEntry("4", 4);
-    glutAddMenuEntry("5", 5);
-    glutAddMenuEntry("6", 6);
-    glutAddMenuEntry("7", 7);
-    glutAddMenuEntry("8", 8);
-    glutAddMenuEntry("9", 9);
-    glutAddMenuEntry("10", 10);
-    glutAddMenuEntry("Exit", -1);
-    glutAttachMenu(GLUT_RIGHT_BUTTON);
-
+    srand((unsigned int)time(NULL));
     glutDisplayFunc(display);
     glutMainLoop();
 }
